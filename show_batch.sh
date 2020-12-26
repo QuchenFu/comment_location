86fa6dc32ca0dd1d7270e00030f6e146578451aa
Quchen FU <hbwhfqc@gmail.com>
2020-12-25 23:34:11 -0600

    update

--- show_batch.sh
+++ show_batch.sh
@@ -1,9 +1,5 @@
-git clone https://github.com/QuchenFu/comment_location.git
-
-cd comment_location
+#!/bin/bash
 
 pip3 install -r requirements.txt
 
-python3 show_batch.py --batch_size=2 --file_path="split30_word_data/train/data.txt"
-
-python3 show_batch.py --batch_size=8 --file_path="split30_word_data/train/data.txt"
+python3 show_batch.py --batch_size=$1 --file_path=$2
